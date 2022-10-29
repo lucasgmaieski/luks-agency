858c716330ce8f4db178ae1d2f7f64261c7aa55d
Lucas Maieski <lucasgrigol@gmail.com>
2022-10-29 19:53:17 -0300

    adicionando contagem no circulo de progresso

--- js/scripts.js
+++ js/scripts.js
@@ -21,39 +21,35 @@
 // });
 // var ProgressBar = require('progressbar.min.js');
 
-// window.onload = function () {
-//     //Progress bar
-//         let containerA = document.getElementById("circleA");
-    
-//         let circleA = new ProgressBar.Circle(containerA, {
-//             color: '#64daf9',
-//             strokeWidth: 8,
-//             duration: 1400,
-//             from: {color: '#AAA'},
-//             to: { color: '#65daf9'},
-    
-//             step: function(state, circle) {
-//                 circleA.path.setAttribute('stroke', state.color);
-    
-//                 let value = Math.round(circle.value() * 60 );
-//                 circleA.setText(value)
-//             }
-//         });
-    
-//         circleA.animate(1.0);
-//     }
 const circleAnime = document.getElementById('data-area');
+const circle = document.querySelectorAll(".circleProgress");
+    const counter = document.querySelectorAll(".counter");
 let jaAnimou = 0;
 function go() {
-    var circle = document.querySelectorAll(".circleProgress");
-    let cont = 1.1
+    
+    let cont = 1
     circle.forEach( (element, i) => {
+        increment(0, counter[i].getAttribute('value'), counter[i]);
         element.style.strokeDashoffset = 0;
         circle[i+1].style.setProperty("transition-duration", `${cont}s`);
-        cont *=1.7
-
+        cont *=1.3;
+        // console.log(counter[i].getAttribute('value'))
     })
 }
+
+function increment(i, max, element){
+    let time;
+    if(i > max) {
+        // clearTimeout(time)
+        return;
+    }
+    setTimeout(function(){
+        console.log("ele entra aqui mano ?")
+      element.innerText = Math.round(i);
+      if(i <= max) increment(i+(max/100), max, element);
+    },11)
+  }
+
 const debounce = function(func, wait, immediate) {
     let timeout;
     return function(...args) {
@@ -77,12 +73,14 @@ function animeScroll() {
     if(windowTop > circleAnime.offsetTop){
         jaAnimou = 1;
         go();
+        
     }
 }
 //Se ja animou um vez para de ficar ouvindo esse scrooll
 if (jaAnimou === 0) {
         window.addEventListener('scroll',  debounce(function() {
-            animeScroll();
+            if(jaAnimou === 0) animeScroll();
+            else return;
             console.log("entra aqui mil vezes")
             console.log(jaAnimou)
         }, 200))
